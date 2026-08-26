477bd67497611c26c88b03d734966c0020eb55ac
IamPial <pialname394@gmail.com>
2026-08-26 22:05:29 +0600

    render banner section here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Banner from "@/components/homepage/Banner";
+
 const  Home = ()=> {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+    <div>
+      <Banner/>
     </div>
   );
 }
