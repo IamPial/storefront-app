3d84f3ebeb5e143123dc1723a5c3ecba47b0a5d7
IamPial <pialname394@gmail.com>
2026-08-27 00:07:31 +0600

    changes padding styles

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = ()=> {
 
   return (
     <nav className="sticky top-0 z-40 w-full border-b border-separator bg-[#0f172a] backdrop-blur-lg">
-      <header className="mx-auto flex h-16 container items-center justify-between px-6">
+      <header className="mx-auto flex h-16 container items-center justify-between px-6 md:px-4">
         <div className="flex items-center gap-4">
           <button
             className="md:hidden"
