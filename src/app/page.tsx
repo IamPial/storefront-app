d142d9f85f3d3f1a6ca942dcd8332318cc9c7c9a
IamPial <pialname394@gmail.com>
2026-08-27 14:40:31 +0600

    render Newsletter section here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/homepage/Banner";
 import CategorySection from "@/components/homepage/CategorySection";
 import FeaturedSection from "@/components/homepage/FeaturedSection";
+import NewsletterSection from "@/components/homepage/NewsLetterSection";
 import PromoBanner from "@/components/homepage/PromoBanner";
 import ValueFeatures from "@/components/homepage/ValueFeatures";
 
@@ -13,6 +14,7 @@ const  Home = ()=> {
       <CategorySection/>
       <PromoBanner/>
       <ValueFeatures/>
+      <NewsletterSection/>
     </div>
   );
 }
